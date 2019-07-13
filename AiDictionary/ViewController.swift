944a47c0669f19417bc2311ef7ddcd0a2faf2b17
lavaspoon <lavaspoon@naver.com>
2019-07-13 12:37:30 +0900

    2019.07.13 프로젝트 생성

---
+++ AiDictionary/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AiDictionary
+//
+//  Created by lavaspoon on 13/07/2019.
+//  Copyright © 2019 lavaspoon. All rights reserved.
+//
+//프로젝트 생성
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
